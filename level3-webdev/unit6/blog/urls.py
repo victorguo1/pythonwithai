15cb6354841361cb9f121419a609147634d0c70f
victorguo1 <ggwd@hotmail.com>
2021-03-09 21:32:04 -0500

    unit6+login

---
+++ level3-webdev/unit6/blog/urls.py
@@ -0,0 +1,16 @@
+# blog/urls.py
+from django.urls import path
+from .views import (
+    HomePageView,
+    BlogDetailView,
+    BlogCreateView,
+    BlogUpdateView,
+    BlogDeleteView) # new
+
+urlpatterns = [
+    path('post/delete/<int:pk>', BlogDeleteView.as_view(), name='post_delete'), # new
+    path('post/edit/<int:pk>', BlogUpdateView.as_view(), name='post_edit'), # new
+    path('post/new/',BlogCreateView.as_view(), name='post_new'),# new
+    path('post/<int:pk>/', BlogDetailView.as_view(),name='post_detail'), #new
+    path('',HomePageView.as_view(),name = 'home'),
+]
